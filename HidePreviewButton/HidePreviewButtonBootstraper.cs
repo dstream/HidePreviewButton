cff4d2046bc0aaf1df86d2b7f3b3236d7d00f936
Nick Hoang <nick@codebrewery.com>
2018-06-13 15:07:51 +0700

    No js solution

---
+++ HidePreviewButton/HidePreviewButtonBootstraper.cs
@@ -0,0 +1,28 @@
+﻿using Umbraco.Core;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Models.ContentEditing;
+
+namespace Dstream
+{
+    public class HidePreviewButtonBootstraper: ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+
+            EditorModelEventManager.SendingContentModel += (sender, e) =>
+            {
+                var contentModel = e.Model as ContentItemDisplay;
+                if (contentModel != null && AppSettings.Instance.HidePreviewButtonAllowDocTypes.Contains(contentModel.ContentTypeAlias))
+                {
+                    contentModel.AllowPreview = true;
+                }
+                else
+                {
+                    contentModel.AllowPreview = false;
+                }
+            };
+
+        }
+    }
+}
